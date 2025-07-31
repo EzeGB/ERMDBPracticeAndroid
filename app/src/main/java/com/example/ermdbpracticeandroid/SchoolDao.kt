b1ecafc666f132084803986ddd5137b2d06b8265
Zeke <ezequielgerstel777@gmail.com>
2025-07-31 16:08:43 -0400

    School and students dao transactions

--- app/src/main/java/com/example/ermdbpracticeandroid/SchoolDao.kt
+++ app/src/main/java/com/example/ermdbpracticeandroid/SchoolDao.kt
@@ -7,7 +7,9 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.example.ermdbpracticeandroid.entities.Director
 import com.example.ermdbpracticeandroid.entities.School
+import com.example.ermdbpracticeandroid.entities.Student
 import com.example.ermdbpracticeandroid.entities.relations.SchoolAndDirector
+import com.example.ermdbpracticeandroid.entities.relations.SchoolWithStudents
 
 @Dao
 interface SchoolDao {
@@ -18,7 +20,14 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDirector(director: Director)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudent(student: Student)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String) : List<SchoolAndDirector>
+
+    @Transaction
+    @Query("SELECT * FROM school WHERE schoolName = :schoolName")
+    suspend fun getSchoolWithStudents(schoolName: String) : List<SchoolWithStudents>
 }
