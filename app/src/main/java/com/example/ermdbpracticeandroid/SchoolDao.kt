0ac66fd6d4bc09d2032e8af50b0b4dd1cae7b34d
Zeke <ezequielgerstel777@gmail.com>
2025-08-01 14:11:32 -0400

    DAO´s final queries

--- app/src/main/java/com/example/ermdbpracticeandroid/SchoolDao.kt
+++ app/src/main/java/com/example/ermdbpracticeandroid/SchoolDao.kt
@@ -8,8 +8,12 @@ import androidx.room.Transaction
 import com.example.ermdbpracticeandroid.entities.Director
 import com.example.ermdbpracticeandroid.entities.School
 import com.example.ermdbpracticeandroid.entities.Student
+import com.example.ermdbpracticeandroid.entities.Subject
 import com.example.ermdbpracticeandroid.entities.relations.SchoolAndDirector
 import com.example.ermdbpracticeandroid.entities.relations.SchoolWithStudents
+import com.example.ermdbpracticeandroid.entities.relations.StudentSubjectCrossRef
+import com.example.ermdbpracticeandroid.entities.relations.StudentsWithSubjects
+import com.example.ermdbpracticeandroid.entities.relations.SubjectsWithStudents
 
 @Dao
 interface SchoolDao {
@@ -23,6 +27,12 @@ interface SchoolDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertStudent(student: Student)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubject(subject: Subject)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStudentSubjectCrossRef(studentSubjectCrossRef: StudentSubjectCrossRef)
+
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String) : List<SchoolAndDirector>
@@ -30,4 +40,12 @@ interface SchoolDao {
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolWithStudents(schoolName: String) : List<SchoolWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM student WHERE studentName = :studentName")
+    suspend fun getSubjectsOfStudent(studentName: String) : List<StudentsWithSubjects>
+
+    @Transaction
+    @Query("SELECT * FROM subject WHERE subjectName = :subjectName")
+    suspend fun getStudentOfSubjects(subjectName: String) : List<SubjectsWithStudents>
 }
